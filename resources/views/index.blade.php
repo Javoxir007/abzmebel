bb56e17ccadc356753db35754cd6f16b00d00cdc
Normetov Javoxir <Javoxir007>
2022-06-22 22:28:21 +0500

    3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -217,7 +217,7 @@
                         </div>
                         <div class="name-count">
                             <div class="count-img">
-                                <img src="assets/img/german.png" alt="">
+                                <img src="assets/img/de.webp" alt="">
                             </div>
                             <p>{{ __('Germany') }}</p>
                         </div>
